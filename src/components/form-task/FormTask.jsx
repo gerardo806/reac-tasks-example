6caffcc3f5032c4d6ebda7ff7195e0ed2ae06c40
gerardo806 <anayaminolta@yandex.com>
2023-01-06 01:51:18 -0600

    componente formulario creado

---
+++ src/components/form-task/FormTask.jsx
@@ -0,0 +1,14 @@
+function FormTask() {
+  return (
+    <div>
+        <form>
+            <input type="text" placeholder="Escribe tu tarea" />
+            <button>
+                Guardar
+            </button>
+        </form>
+    </div>
+  )
+}
+
+export default FormTask
